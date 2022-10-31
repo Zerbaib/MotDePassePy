ede55a7af88944034a6a76df918af050656dc388
Hugo <Hugo@HUGOPC>
2022-10-31 17:16:45 +0100

    list to dictionnaire and it's good for this

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-list = []
+list = {}
 print("Bienvenue dans mon gestionnaire de mot de passe")
 
 while True:
     a = input("que veux tu faire ?\n1. ajouter un mot de passe\n2. lire un mot de passe\n")
     if a == "1":
-        list = input("ajoute ton mot de passe au format site:mdp\n")
+        b = input("quelle est le site ?\n")
+        c = input("quelle est le mot de passe ?\n")
+        list[b]=c
+        
         print(list)
     if a == "2":
         print("quelle mot de passe veux tu lire")
