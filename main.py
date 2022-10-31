45b8b03e8a8dc7b93f68e4b4501dbc57c97e92fa
Hugo <Hugo@HUGOPC>
2022-10-31 12:09:22 +0100

    *

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ print("Bienvenue dans mon gestionnaire de mot de passe")
 
 while True:
     a = input("que veux tu faire ?\n1. ajouter un mot de passe\n2. lire un mot de passe\n")
-    print(a)
     if a == "1":
-        print("yes")
-    else:
-        print("no")
+        list = input("ajoute ton mot de passe au format site:mdp\n")
+        print(list)
+    if a == "2":
+        print("quelle mot de passe veux tu lire")
+    
