1c17744c5a2aee830c2f112c7095b47946e4561a
Hugo <Hugo@HUGOPC>
2022-10-31 17:24:42 +0100

    good name of variable

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 list = {}
 print("Bienvenue dans mon gestionnaire de mot de passe")
 
+
+
 while True:
-    a = input("que veux tu faire ?\n1. ajouter un mot de passe\n2. lire un mot de passe\n")
-    if a == "1":
-        b = input("quelle est le site ?\n")
-        c = input("quelle est le mot de passe ?\n")
-        list[b]=c
+    choix = input("que veux tu faire ?\n1. ajouter un mot de passe\n2. lire un mot de passe\n")
+    if choix == "1":
+        site = input("quelle est le site ?\n")
+        mdp = input("quelle est le mot de passe ?\n")
+        list[site] = mdp
         
-    if a == "2":
-        e = input("quelle mot de passe veux tu lire\n")
-        print("Ton mot de passe est:\n" + list.get(e)) 
+    if choix == "2":
+        see = input("quelle mot de passe veux tu lire\n")
+        print("Ton mot de passe est:\n" + list.get(see))
