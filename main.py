59da9d6d0d0e6473e82bb2fc5629cb3f98634367
Hugo <Hugo@HUGOPC>
2022-10-31 12:04:56 +0100

    if fini

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ print("Bienvenue dans mon gestionnaire de mot de passe")
 
 while True:
     a = input("que veux tu faire ?\n1. ajouter un mot de passe\n2. lire un mot de passe\n")
-    if a == 1:
+    print(a)
+    if a == "1":
         print("yes")
     else:
         print("no")
