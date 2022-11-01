1902fed3d8b618d6a4efb4b70a498a5cc8e89b7d
Hugo <Hugo@HUGOPC>
2022-11-01 08:58:06 +0100

    open def end json save

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import json
+
+def addMDP():
+    site = input("quelle est le site ?\n")
+    mdp = input("quelle est le mot de passe ?\n")
+    list[site] = mdp
+
+def seeMDP():
+    see = input("quelle mot de passe veux tu lire\n")
+    print("Ton mot de passe est:\n" + list.get(see))
+
+def saveMDP():
+    with open('listMDP.txt', 'w') as file:
+        list = json.load(list, file)
+    print("Mot de passe sauvegarder")
+
 list = {}
 print("Bienvenue dans mon gestionnaire de mot de passe")
 
 while True:
     choix = input("que veux tu faire ?\n1. ajouter un mot de passe\n2. lire un mot de passe\n")
     if choix == "1":
-        site = input("quelle est le site ?\n")
-        mdp = input("quelle est le mot de passe ?\n")
-        list[site] = mdp
-        
+        addMDP()
+
     if choix == "2":
-        see = input("quelle mot de passe veux tu lire\n")
-        print("Ton mot de passe est:\n" + list.get(see))
+        seeMDP()
+
+    if choix == "3":
+        saveMDP()
